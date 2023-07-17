337ebe74aacbc9096da39ccc1d7892452d787437
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-17 02:09:57 -0300

    feat:vistas cambiadas clientes cambiada con bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,8 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
